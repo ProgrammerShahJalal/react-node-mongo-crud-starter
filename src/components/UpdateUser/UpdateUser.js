e942b3dba998c9685b24fc14e48f60f4e86937da
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-24 01:13:01 +0600

    mongo crud starter

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const UpdateUser = () => {
+    const [user, setUser] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/users/${id}`)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, [id])
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update {user.name}</h2>
+            <p><small>{id}</small></p>
         </div>
     );
 };
